60babe68ba5a1be21c39936b09868bb081183632
Samuel Watson <samuel.watson@bigmarketing.co.uk>
2020-01-07 16:03:35 +0000

    Removed and updated secret

--- primal_silver_store/settings.py
+++ primal_silver_store/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^%ihwndy4nbp0ew2io5j@^q9q2a-bdpgmf@axt41=%h=dp$e*%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
