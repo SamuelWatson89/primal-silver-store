5c7e345596a5860ff7b611cafb7aeb73df06e8b4
Samuel <otacon100@hotmail.com>
2020-01-07 22:22:20 +0000

    Updated styling and started adding shopping cart

---
+++ cart/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect, reverse
+
+
+def view_cart(request):
+    """
+    View that renders the cart contents
+    """
+    return render(request, 'cart.html')
+
+
+def add_to_cart(request, id):
+    """
+    Add a quantity of the product to the cart
+    """
+    quantity = int(request.POST.get('quantity'))
+
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """
+    Adjust the amount of the item in the cart
+    """
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
--- primal_silver_store/urls.py
+++ primal_silver_store/urls.py
@@ -2,10 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from accounts.views import index
 from accounts import urls as accounts_urls
+from products import urls as products_urls
+from cart import urls as cart_urls
+from products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name="index"),
-    path('accounts/', include(accounts_urls))
+    path('', all_products, name="index"),
+    path('accounts/', include(accounts_urls)),
+    path('products/', include(products_urls)),
+    path('cart/', include(cart_urls)),
+    path('media/(<path>.*)/', static.serve, {'document_root": MEDIA_ROOT'})
 ]
