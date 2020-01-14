4237e58d9efd8b8cf5493fab96b51dd6b48cbe7f
Samuel <otacon100@hotmail.com>
2020-01-14 22:17:10 +0000

    Completed checkout app, added stripe payment api and started heroku setup

--- primal_silver_store/urls.py
+++ primal_silver_store/urls.py
@@ -5,6 +5,7 @@ from accounts import urls as accounts_urls
 from products import urls as products_urls
 from cart import urls as cart_urls
 from search import urls as search_urls
+from checkout import urls as checkout_urls
 from products.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
@@ -19,5 +20,6 @@ urlpatterns = [
     path('products/', include(products_urls)),
     path('cart/', include(cart_urls)),
     path('search/', include(search_urls)),
+    path('checkout/', include(checkout_urls)),
     # path('media/<path>.', static.serve, {'document_root": MEDIA_ROOT'}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
