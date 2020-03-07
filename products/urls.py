03db59564f3731bbee3328b3433778da8093f5bf
Samuel <otacon100@hotmail.com>
2020-03-07 15:40:04 +0000

    Added ability to filter by nav items

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import all_products, a_product
+from .views import all_products, a_product, rings, bracelets, necklaces
 
 
 urlpatterns = [
     path('', all_products, name='products'),
-    path('product/?P<id>', a_product, name='product'),
+    path('?P<id>', a_product, name='product'),
+    path('rings/', rings, name='rings'),
+    path('necklaces/', necklaces, name='necklaces'),
+    path('bracelets/', bracelets, name='bracelets'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,3 +10,18 @@ def all_products(request):
 def a_product(request, id):
     product = Product.objects.get(id=id)
     return render(request, 'product.html', {"product": product})
+
+
+def rings(request):
+    products = Product.objects.filter(product_type__name__iexact="ring")
+    return render(request, "products.html", {"products": products})
+
+
+def necklaces(request):
+    products = Product.objects.filter(product_type__name__iexact="necklace")
+    return render(request, "products.html", {"products": products})
+
+
+def bracelets(request):
+    products = Product.objects.filter(product_type__name__iexact="bracelet")
+    return render(request, "products.html", {"products": products})
