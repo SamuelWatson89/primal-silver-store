f4659395b3cb34f02b9658e08e414533860e05a5
Samuel <otacon100@hotmail.com>
2020-03-07 13:22:52 +0000

    Added product type class, mapped to product clas

--- products/admin.py
+++ products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Product_types
 
 admin.site.register(Product)
+admin.site.register(Product_types)
--- products/models.py
+++ products/models.py
@@ -1,11 +1,23 @@
 from django.db import models
 
 
+class Product_types(models.Model):
+    name = models.CharField(max_length=254, default='')
+    
+    class Meta:
+        verbose_name = 'Product Type'
+        verbose_name_plural = 'Product Types'
+    
+    def __str__(self):
+        return self.name.replace('_', ' ')
+
+
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+    product_type = models.ForeignKey(Product_types, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
-        return self.name
+        return self.name.replace('_', ' ')
