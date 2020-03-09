8265a8637ccce9215b14e57133b8eaf4b5ff6987
Samuel <otacon100@hotmail.com>
2020-03-09 20:15:09 +0000

    Added some bootstrap styling to login, register and password reset pages

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -3,10 +3,8 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 
 urlpatterns = [
-    path('', PasswordResetView.as_view(), name='password_reset'),
-    path('done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
-         name='password_reset_confirm'),
-    path('complete', PasswordResetCompleteView.as_view(),
-         name='password_reset_complete')
-]
+     path('', PasswordResetView.as_view(), name='password_reset'),
+     path('done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+     path('complete', PasswordResetCompleteView.as_view(), name='password_reset_complete')
+     ]
