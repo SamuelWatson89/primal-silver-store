432cde8a6ba885f94d9e723ce239f41a00147eab
Samuel <otacon100@hotmail.com>
2020-04-11 17:52:22 +0100

    Added stock count column, implemented the feature to the front end, displays whether the item is in stock or not. Back end needs work to update when item is purchased.

--- products/models.py
+++ products/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 class Product_types(models.Model):
     name = models.CharField(max_length=254, default='')
-    
+
     class Meta:
         verbose_name = 'Product Type'
         verbose_name_plural = 'Product Types'
-    
+
     def __str__(self):
         return self.name.replace('_', ' ')
 
@@ -16,8 +16,11 @@ class Product(models.Model):
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock_count = models.DecimalField(
+        max_digits=3, decimal_places=0, default=0)
     image = models.ImageField(upload_to='images')
-    product_type = models.ForeignKey(Product_types, on_delete=models.PROTECT, null=True)
+    product_type = models.ForeignKey(
+        Product_types, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.name.replace('_', ' ')
