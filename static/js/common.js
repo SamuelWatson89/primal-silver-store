966edd0c44e20c6c6e98d643c60b2a7b42fc3cf7
Samuel <otacon100@hotmail.com>
2020-04-13 15:58:10 +0100

    Added logic to show which items are trying to be over ordered. Disabled the checkout button if items are over ordered

--- static/js/common.js
+++ static/js/common.js
@@ -1,7 +1,14 @@
-var message_ele = document.getElementById("message_container");
+const message_ele = document.getElementById("message_container");
+const over_stock = document.getElementsByClassName("itemOverOrder");
 
 if (message_ele) {
   setTimeout(function () {
     message_ele.classList.add("message-exit");
   }, 3000);
+}
+
+if (over_stock.length > 0) {
+  var checkout_button = document.getElementById("checkoutButton");
+  checkout_button.disabled = true;
+  checkout_button.classList.add("disabled");
 }
