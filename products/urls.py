b668e08686d93a46f597e6b36139ef2b5237824d
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-27 16:20:19 +0100

    Updated to include and all producst page

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path, include
-from .views import all_products, a_product, rings, bracelets, necklaces, earrings
+from django.urls import path
+from .views import (
+    all_products,
+    a_product,
+    rings,
+    bracelets,
+    necklaces,
+    earrings
+    )
 
 
 urlpatterns = [
-    path('', all_products, name='products'),
-    path('?P<id>', a_product, name='product'),
+    path('all/', all_products, name='products'),
+    path('<int:id>', a_product, name='product'),
     path('rings/', rings, name='rings'),
     path('necklaces/', necklaces, name='necklaces'),
     path('bracelets/', bracelets, name='bracelets'),
