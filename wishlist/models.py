6a89c4d71f873d11cdf3f66d773bd15c338a10a8
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:04:50 +0100

    Remove unused imports and fix indentaion

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
-from django.utils import timezone
 
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(User, related_name='user',
-                             on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(
+        User,
+        related_name='user',
+        on_delete=models.CASCADE, null=True
+        )
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
