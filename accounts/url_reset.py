c19ebd4690b9488bbc8a00accd136fe4c8ae6b2d
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:49:20 +0100

    Fixed formatting of lines

--- accounts/url_reset.py
+++ accounts/url_reset.py
@@ -1,10 +1,23 @@
 from django.urls import path
-from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.contrib.auth.views import (
+     PasswordResetView,
+     PasswordResetDoneView,
+     PasswordResetConfirmView,
+     PasswordResetCompleteView
+)
 
 urlpatterns = [
      path('', PasswordResetView.as_view(), name='password_reset'),
-     path('done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-     path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-     path('complete', PasswordResetCompleteView.as_view(), name='password_reset_complete')
+     path(
+          'done/', PasswordResetDoneView.as_view(),
+          name='password_reset_done'
+          ),
+     path(
+          'reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(),
+          name='password_reset_confirm'
+          ),
+     path(
+          'complete', PasswordResetCompleteView.as_view(),
+          name='password_reset_complete'
+          )
      ]
