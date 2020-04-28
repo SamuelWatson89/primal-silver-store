c0d96cedbeb25bccec96a9d317d83090699fb67e
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:18:48 +0100

    Added docstring to search app

--- search/views.py
+++ search/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from products.models import Product
 
+
 # Create your views here.
 def do_search(request):
+    """
+    Function to search the database for the website
+    searching for a product that contains the search query
+    """
     products = Product.objects.filter(name__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
