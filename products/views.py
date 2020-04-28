dc5bc80b8444c4d8e9ae6c3a0c8ab2ba1bc44b4b
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:26:55 +0100

    Added docstrings for product views

--- products/views.py
+++ products/views.py
@@ -4,6 +4,9 @@ from .models import Product, Product_types
 
 
 def all_products(request):
+    """
+    Create view to get all products and render the proucts page
+    """
     product_list = Product.objects.all()
     page = request.GET.get('page', 1)
 
@@ -18,20 +21,25 @@ def all_products(request):
 
 
 def a_product(request, id):
+    """
+    Create view to get a product by id and display just that one
+    """
     product = Product.objects.get(id=id)
     return render(request, 'product.html', {"product": product})
 
 
 def product_types(request):
+    """
+    Create view to get a product types
+    """
     product_types = Product_types.object.all()
     return {'product_types': product_types}
 
 
 def product_stock_count(request):
     """
-    Global access to the product stock counts
+    Create a view to get product stock levels
     """
-
     products = Product.objects.all()
     for product in products:
         product_stock = product.stock_count
@@ -39,20 +47,32 @@ def product_stock_count(request):
 
 
 def rings(request):
+    """
+    Create view to get a product by type of ring
+    """
     products = Product.objects.filter(product_type__name__iexact="ring")
     return render(request, "products.html", {"products": products})
 
 
 def necklaces(request):
+    """
+    Create view to get a product by type of necklace
+    """
     products = Product.objects.filter(product_type__name__iexact="necklace")
     return render(request, "products.html", {"products": products})
 
 
 def bracelets(request):
+    """
+    Create view to get a product by type of bracelet
+    """
     products = Product.objects.filter(product_type__name__iexact="bracelet")
     return render(request, "products.html", {"products": products})
 
 
 def earrings(request):
+    """
+    Create view to get a product by type of earring
+    """
     products = Product.objects.filter(product_type__name__iexact="earring")
     return render(request, "products.html", {"products": products})
