401284c6be41cf9442c730e1056a36985c6390ae
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:30:31 +0100

    Addded doc string to views for landing page

--- primal_silver_store/views.py
+++ primal_silver_store/views.py
@@ -3,6 +3,10 @@ from products.models import Product, Product_types
 
 
 def landing(request):
+    """
+    View to create the landing page and
+    get the required information from other app
+    """
     products = Product.objects.all().order_by('?')[:5]
     product_types = Product_types.objects.all()
     return render(
