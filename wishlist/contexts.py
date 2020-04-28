12b482ae0812881a0e5dd50039635ab62dca0046
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:02:26 +0100

    Removed unused imports and vaiables

--- wishlist/contexts.py
+++ wishlist/contexts.py
@@ -1,7 +1,4 @@
-from django.shortcuts import get_object_or_404
 from wishlist.models import Wishlist
-from django.contrib.auth.models import User
-from django.template import Context
 
 
 def wishlist_contents(request):
@@ -9,7 +6,6 @@ def wishlist_contents(request):
     Ensure that the users wishlist contents are available on every page
     """
     if request.user.is_authenticated:
-        user = User.objects.get(email=request.user.email)
         wishlist = Wishlist.objects.filter(user_id=request.user.id)
         wishlist_count = wishlist.count()
         return {"wishlist_count": wishlist_count}
