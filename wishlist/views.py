a0319dd2ffaca5be8129c8bdf49fe8aa920502dd
Sam Watson <samuel.watson@bigmarketing.co.uk>
2020-04-28 20:08:14 +0100

    Fixed indentation and avoided long lines

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import auth, messages
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from products.models import Product
@@ -14,7 +14,11 @@ def user_wishlist(request):
     user = User.objects.get(email=request.user.email)
     wishlist = Wishlist.objects.filter(user_id=request.user.id)
     wishlist_count = wishlist.count()
-    return render(request, 'wishlist.html', {"profile": user, "wishlist": wishlist, "wishlist_count": wishlist_count})
+    return render(request, 'wishlist.html', {
+        "profile": user,
+        "wishlist": wishlist,
+        "wishlist_count": wishlist_count
+        })
 
 
 @login_required
@@ -24,9 +28,10 @@ def add_to_wishlist(request, id):
     """
     item = get_object_or_404(Product, pk=id)
 
-    product, date_added = Wishlist.objects.get_or_create(product=item,
-                                                         user=request.user,
-                                                         )
+    product, date_added = Wishlist.objects.get_or_create(
+        product=item,
+        user=request.user,
+    )
 
     messages.info(request, 'The item was added to your wishlist')
     return redirect(reverse('index'))
